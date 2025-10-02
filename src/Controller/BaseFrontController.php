0bebcc4fbd02489b4dbf6ef723b5d5b0cac2a8f2
Bobillier Michel <michel.bobillier@etat.ge.ch>
2025-10-02 18:12:47 +0200

    fix: correct indentation in BaseFrontController constructor

--- src/Controller/BaseFrontController.php
+++ src/Controller/BaseFrontController.php
@@ -7,7 +7,7 @@ use Psr\Log\LoggerInterface;
 
 class BaseFrontController extends BaseController
 {
-public function __construct(Menu $menu, LoggerInterface $applicationLogger)
+    public function __construct(Menu $menu, LoggerInterface $applicationLogger)
     {
         $this->menuTwig = $menu->menuTwig;
         $this->logger = $applicationLogger;
