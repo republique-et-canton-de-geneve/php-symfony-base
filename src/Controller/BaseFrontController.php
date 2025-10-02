97112696267b61d98dd97e3365261253ef8016e7
Bobillier Michel <michel.bobillier@etat.ge.ch>
2025-10-02 18:10:59 +0200

    feat: implement constructor for BaseFrontController to initialize menu and logger

--- src/Controller/BaseFrontController.php
+++ src/Controller/BaseFrontController.php
@@ -2,6 +2,14 @@
 
 namespace App\Controller;
 
+use App\Menu\Menu;
+use Psr\Log\LoggerInterface;
+
 class BaseFrontController extends BaseController
 {
+public function __construct(Menu $menu, LoggerInterface $applicationLogger)
+    {
+        $this->menuTwig = $menu->menuTwig;
+        $this->logger = $applicationLogger;
+    }
 }
