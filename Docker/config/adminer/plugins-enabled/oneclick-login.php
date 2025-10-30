6ad9bb89b90e595c0d52cf103867d24701be32cd
Bobillier Michel <michel.bobillier@etat.ge.ch>
2025-10-30 11:18:13 +0100

    feat:  Docker

---
+++ Docker/config/adminer/plugins-enabled/oneclick-login.php
@@ -0,0 +1,3 @@
+<?php
+require_once('plugins/oneclick-login.php');
+return new OneClickLogin(include 'servers.php');
---
+++ Docker/config/adminer/servers.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'mariadb' => array(
+        // Required parameters
+        'username'  => 'root',
+        'pass'      => 'root',
+        'driver'   => 'server',
+        'label'     => 'Mariadb',
+    ),
+    'mysql' => array(
+        // Required parameters
+        'username'  => 'root',
+        'pass'      => 'root',
+        'driver'   => 'server',
+        'label'     => 'MySql',
+    ),
+    'postgres' => array(
+        // Required parameters
+        'username'  => 'root',
+        'pass'      => 'root',
+        'driver'   => 'pgsql',
+        'label'     => 'PostgreSQL',
+    ),
+
+];
