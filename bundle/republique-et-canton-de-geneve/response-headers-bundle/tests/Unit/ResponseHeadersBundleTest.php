e234ddc8be4b2e32b75cba5183e55a1a6e87abce
Bobillier Michel <michel.bobillier@etat.ge.ch>
2025-12-09 14:44:21 +0100

    feat: phpUnit add test

--- bundle/republique-et-canton-de-geneve/response-headers-bundle/tests/Unit/ResponseHeadersBundleTest.php
+++ bundle/republique-et-canton-de-geneve/response-headers-bundle/tests/Unit/ResponseHeadersBundleTest.php
@@ -6,6 +6,8 @@ use EtatGeneve\ResponseHeadersBundle\ResponseHeadersBundle;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\Config\Definition\Loader\DefinitionFileLoader;
+use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -14,84 +16,47 @@ use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 class ResponseHeadersBundleTest extends TestCase
 {
 
-    private ResponseHeadersBundle $reponseHeaderBundle ;
-    public function setUp(): void {
-                $this->reponseHeaderBundle =  new ResponseHeadersBundle();
-
+    private ResponseHeadersBundle $responseHeadersBundle;
+    public function setUp(): void
+    {
+        $this->responseHeadersBundle =  new ResponseHeadersBundle();
     }
-
-
-    // public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
-    // {
-    //     $id = 'response_headers.response_listener';
-    //     $services = $containerConfigurator->services();
-    //     $services->set($id, ResponseListener::class)
-    //     ->arg('$headers', $config['headers'])
-    //     ->tag('kernel.event_listener', ['event' => 'kernel.response', 'method' => 'onKernelResponse'])
-    //     ;
-    // }
-
-
 
 
     public function testLoadExtension(): void
     {
-        $config = [];
+        $config = ['headers' => []];
         $containerBuilder = new ContainerBuilder();
         $phpFileLoader = new PhpFileLoader($containerBuilder, new FileLocator(\dirname(__DIR__) . '/Resources/config'));
         $instanceOf = [];
         $containerConfigurator = new ContainerConfigurator($containerBuilder, $phpFileLoader, $instanceOf, 'xx', 'xx');
-        $this->reponseHeaderBundle->loadExtension($config, $containerConfigurator, $containerBuilder);
+        $this->responseHeadersBundle->loadExtension($config, $containerConfigurator, $containerBuilder);
         $this->assertTrue(true);
     }
 
 
 
-    public function testConfigure() {
+    public function testConfigure()
+    {
+        $treeBuilder = new TreeBuilder('response_header');
+        $fileLocator = new  FileLocator();
+        $defintionLoader = new DefinitionFileLoader($treeBuilder, $fileLocator);
+        $definition = new DefinitionConfigurator($treeBuilder, $defintionLoader, '', '');
+        $this->responseHeadersBundle->configure($definition);
+        $this->assertTrue(true);
+        $node = $definition->rootNode()->getNode(true);
 
 
-        // $treeBuilder = new TreeBuilder();
-        // $definition = new DefinitionConfigurator();
+        $processor = new Processor();
+        $processor->process($node, [
+            0 => [
+                'headers' =>
+                [
+                    'x1' => ['value' => 'val1'],
+                    'x2' => "toto",
+                    'x3' => ['a', 'b']
+                ]
+            ]
+        ]);
     }
-
-    // public function configure(DefinitionConfigurator $definition): void
-    // {
-    //     $definition->rootNode()
-    //         ->children()
-    //         ->arrayNode('headers')
-    //         ->useAttributeAsKey('name')
-    //         ->arrayPrototype()
-    //         ->beforeNormalization()
-    //         ->ifString()
-    //         ->then(function (string $v): array {
-    //             return ['value' => $v];
-    //         })
-    //         ->end()
-    //         ->beforeNormalization()
-    //         ->ifArray()
-    //         ->then(function (array $v): array {
-    //             if (array_keys($v) === range(0, count($v) - 1)) {
-    //                 return ['value' => $v];
-    //             }
-
-    //             return $v;
-    //         })
-    //         ->end()
-    //         ->children()
-    //         ->arrayNode('value')
-    //         ->beforeNormalization()
-    //         ->ifString()
-    //         ->then(function (string $v): array {
-    //             return ['value' => $v];
-    //         })
-    //         ->end()
-    //         ->scalarPrototype()->end()
-    //         ->end()
-    //         ->scalarNode('condition')->end()
-    //         ->end()
-    //         ->end()
-    //         ->end()
-    //         ->end()
-    //     ;
-    // }
 }
